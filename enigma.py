9c70de90ceb584d41d3d45db8fb87ea85d9787cb
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2026-06-28 18:52:29 +0200

    Encoding works properly with all rotor's positions

--- enigma.py
+++ enigma.py
@@ -17,36 +17,38 @@ class M1:
         pos1 = self.Rotors[2][ord(s)+self.pos[2]-self.character_offset_factor]
         pos1 = chr(((ord(pos1)-self.character_offset_factor)+26-self.pos[2])%26 + self.character_offset_factor)
 
-        print(pos1)
+        # print(pos1)
 
         pos2 = self.Rotors[1][ord(pos1) + self.pos[1] - self.character_offset_factor]
         pos2 = chr(((ord(pos2) - self.character_offset_factor) + 26 - self.pos[1]) % 26 + self.character_offset_factor)
-        print(pos2)
+        # print(pos2)
 
         pos3 = self.Rotors[0][ord(pos2) + self.pos[0] - self.character_offset_factor]
         pos3 = chr(((ord(pos3) - self.character_offset_factor) + 26 - self.pos[0]) % 26 + self.character_offset_factor)
-        print(pos3)
+        # print(pos3)
 
         reflected = self.reflector[ord(pos3) - self.character_offset_factor]
-        print(reflected)
+        # print(reflected)
 
-        pos4 = chr(self.Rotors[0].index(reflected) + self.character_offset_factor)
-        print(pos4)
+        pos4 = chr((ord(reflected) - self.character_offset_factor + self.pos[0] + 26) % 26 + self.character_offset_factor)
+        pos4 = chr((self.Rotors[0].index(pos4) - self.pos[0] + 26) % 26 + self.character_offset_factor)
+        # print(pos4)
 
-        pos5 = chr(self.Rotors[1].index(pos4) + self.character_offset_factor)
-        print(pos5)
+        pos5 = chr((ord(pos4) - self.character_offset_factor + self.pos[1] + 26) % 26 + self.character_offset_factor)
+        pos5 = chr((self.Rotors[1].index(pos5) - self.pos[1] + 26) % 26 + self.character_offset_factor)
+        # print(pos5)
 
-        pos6 = chr(self.Rotors[2].index(pos5) + self.character_offset_factor)
+        pos6 = chr((ord(pos5) - self.character_offset_factor + self.pos[2] + 26) % 26 + self.character_offset_factor)
+        pos6 = chr((self.Rotors[2].index(pos6) - self.pos[2] + 26) % 26 + self.character_offset_factor)
         print(pos6)
 
 
 
 
-machine = M1([I, II, III], [0, 0, 1], UKW_B)
+machine = M1([I, II, III], [1, 1, 2], UKW_B)
 
 machine.Individual_Input('A')
 
-print(0%26)
 
 
 
