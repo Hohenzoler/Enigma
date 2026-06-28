52ebde1324caf145f081c9f60f03496355847662
Hohenzoler <127593783+Hohenzoler@users.noreply.github.com>
2026-06-28 18:12:12 +0200

    reworked the encoding: + Initial encoding + reflector works as inteneded - The secound encoding is still broken

--- enigma.py
+++ enigma.py
@@ -8,110 +8,46 @@ class M1:
         self.reflector = reflector
         self.character_offset_factor = 65
         self.turn_3 = False
+        self.ring_position = [0, 0, 0]
 
 
     def Individual_Input(self, s):
-        s = s.capitalize()
+        s = s.upper()
 
-        if self.pos[2] == ord(self.Rotors[2][-1]) + 1 - self.character_offset_factor:
-            self.pos[1] += 1
-            self.turn_3 = True
-            if self.pos[1] == 27:
-                self.pos[1] = 1
+        pos1 = self.Rotors[2][ord(s)+self.pos[2]-self.character_offset_factor]
+        pos1 = chr(((ord(pos1)-self.character_offset_factor)+26-self.pos[2])%26 + self.character_offset_factor)
 
+        print(pos1)
 
+        pos2 = self.Rotors[1][ord(pos1) + self.pos[1] - self.character_offset_factor]
+        pos2 = chr(((ord(pos2) - self.character_offset_factor) + 26 - self.pos[1]) % 26 + self.character_offset_factor)
+        print(pos2)
 
+        pos3 = self.Rotors[0][ord(pos2) + self.pos[0] - self.character_offset_factor]
+        pos3 = chr(((ord(pos3) - self.character_offset_factor) + 26 - self.pos[0]) % 26 + self.character_offset_factor)
+        print(pos3)
 
-        if self.pos[1] == ord(self.Rotors[1][-1]) - self.character_offset_factor and self.turn_3:
-            self.pos[0] += 1
-            self.turn_3 = False
-            if self.pos[0] == 27:
-                self.pos[0] = 1
+        reflected = self.reflector[ord(pos3) - self.character_offset_factor]
+        print(reflected)
 
+        pos4 = chr(self.Rotors[0].index(reflected) + self.character_offset_factor)
+        print(pos4)
 
-        self.pos[2] += 1
-        if self.pos[2] == 27:
-            self.pos[2] = 1
+        pos5 = chr(self.Rotors[1].index(pos4) + self.character_offset_factor)
+        print(pos5)
 
-
-
-        value = ord(s) - self.character_offset_factor + self.pos[2] - 1
-        if value > 25:
-            value %= 26
-        elif value < 0:
-            value += 26
-
-        s = self.Rotors[2][value]
-        print(s)
+        pos6 = chr(self.Rotors[2].index(pos5) + self.character_offset_factor)
+        print(pos6)
 
 
-        value = ord(s) - self.character_offset_factor - self.pos[2] + self.pos[1]
-        if value > 25:
-            value %= 26
-        elif value < 0:
-            value += 26
-        s = self.Rotors[1][value]
-        print(s)
-
-        value = ord(s) - self.character_offset_factor - self.pos[1] + self.pos[0]
-        if value > 25:
-            value %= 26
-        elif value < 0:
-            value += 26
-        s = self.Rotors[0][value]
-        print(s)
-
-        value = ord(s) - self.character_offset_factor - self.pos[0] + 1
-        if value > 25:
-            value %= 26
-        elif value < 0:
-            value += 26
-        s = self.reflector[value]
-        print(s)
-
-        temp_ord = ord(s)+self.pos[0]-1
-
-        if temp_ord > 90:
-            temp_ord = temp_ord%90 + self.character_offset_factor-1
-        elif temp_ord < 65:
-            temp_ord += 26
-
-        reflected_value = self.Rotors[0].index(chr(temp_ord)) + self.character_offset_factor + self.pos[1] - 1
-
-        if reflected_value > 90:
-            reflected_value = reflected_value%90 + self.character_offset_factor-1
-        elif reflected_value < 65:
-            reflected_value += 26
 
 
-        s = chr(reflected_value)
-        print(s)
-        reflected_value = self.Rotors[1].index(s) + self.character_offset_factor + self.pos[2] - 1
-
-        if reflected_value > 90:
-            reflected_value = reflected_value%90 + self.character_offset_factor-1
-        elif reflected_value < 65:
-            reflected_value += 26
-
-        s = chr(reflected_value)
-        print(s)
-
-        reflected_value = self.Rotors[2].index(s) + self.character_offset_factor - self.pos[2] + 1
-        if reflected_value > 90:
-            reflected_value = reflected_value%90 + self.character_offset_factor-1
-        elif reflected_value < 65:
-            reflected_value += 26
-
-        s = chr(reflected_value)
-
-        print(s)
-
-machine = M1([III, II, I], [2, 1, 1], UKW_B)
+machine = M1([I, II, III], [0, 0, 1], UKW_B)
 
 machine.Individual_Input('A')
-machine.Individual_Input('A')
 
-print(machine.pos)
+print(0%26)
+
 
 
 
